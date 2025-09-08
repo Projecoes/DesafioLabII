55a2e0a06fea3c9d9e6c4be0b356bfdd10ea330c
Projecoes <andrewcoelho2507@gmail.com>
2025-09-08 13:11:30 -0300

    Initial commit

---
+++ src/Etapa1/Labirinto.java
@@ -0,0 +1,63 @@
+package Etapa1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Labirinto {
+    private final char PAREDE = 'X';
+    private final char CAMINHO_ABERTO = ' ';
+    private final char SAIDA = 'D';
+    private final char CAMINHO_SOLUCAO = '#';
+
+    private char[][] labirinto;
+
+
+
+    /**
+     * Lê um arquivo de texto contendo a representação de um labirinto.
+     * Conta o número de linhas e determina o maior número de colunas,
+     * para alocar corretamente a matriz que representará o labirinto.
+     *
+     * @param filename o nome do arquivo que contém o labirinto
+     */
+    public void criaLabirinto(String filename) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
+            // LINHA NO SINGULAR é string
+            String linha;
+            //LINHAS no plural é int
+            int linhas = 0;
+            int colunas = 0;
+
+            // Conta as linhas e colunas
+            while ((linha = bufferedReader.readLine()) != null) {
+                linhas++;
+                // Atualiza o número de colunas com base na linha mais longa encontrada até agora
+                colunas = Math.max(colunas, linha.length());
+            }
+
+            labirinto = new char[linhas][colunas];
+
+            bufferedReader.close();
+
+            // Abre um novo BufferedReader para ler o arquivo novamente
+            BufferedReader bufferedReader2 = new BufferedReader(new FileReader(filename));
+            int i = 0;
+
+            // Lê cada linha do arquivo até o fim
+            while ((linha = bufferedReader2.readLine()) != null) {
+                // Para cada caractere da linha atual...
+                for (int j = 0; j < linha.length(); j++) {
+                    // Copia o caractere da string para a matriz labirinto na posição [i][j]
+                    labirinto[i][j] = linha.charAt(j);
+                }
+                i++;
+            }
+
+            bufferedReader2.close();
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+    }
+
+}
---
+++ src/Etapa1/PrincipalLabirinto.java
@@ -0,0 +1,9 @@
+package Etapa1;
+
+public class PrincipalLabirinto {
+    public static void main(String[] args) {
+        Labirinto labirinto = new Labirinto();
+
+        labirinto.criaLabirinto("labirinto.txt");
+    }
+}
