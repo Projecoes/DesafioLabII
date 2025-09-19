b44e8c112c557dd9464b9e5fcc5e1f56474e0403
Projecoes <andrewcoelho2507@gmail.com>
2025-09-19 11:36:24 -0300

    Método pesquisaBinariaCandidatos OK.

--- src/Etapa2/OrdenarCandidatos.java
+++ src/Etapa2/OrdenarCandidatos.java
@@ -5,8 +5,8 @@ public class OrdenarCandidatos {
     public static void ordenaCandidatosPorNome(Candidato[] candidatos) {
         for (int i = 1; i < candidatos.length; i++) {
             Candidato chave = candidatos[i];
-            int j = i-1;
-            while (j >= 0 && candidatos[j].getNome().compareToIgnoreCase(chave.getNome())> 0) {
+            int j = i - 1;
+            while (j >= 0 && candidatos[j].getNome().compareToIgnoreCase(chave.getNome()) > 0) {
                 candidatos[j + 1] = candidatos[j];
                 j--;
             }
@@ -29,13 +29,35 @@ public class OrdenarCandidatos {
         }
     }
 
-    public static void ordenaCandidatosPorPartido(Candidato[] candidato) {
+    public static void ordenaCandidatosPorPartido(Candidato[] candidatos) {
 
+        for (int i = 1; i < candidatos.length; i++) {
+            Candidato chave = candidatos[i];
+            int j = i - 1;
+
+            while (j >= 0 && candidatos[j].getPartido().compareToIgnoreCase(chave.getPartido()) > 0) {
+                candidatos[j + 1] = candidatos[j];
+                j--;
+            }
+            candidatos[j + 1] = chave;
+        }
     }
 
-    public static int pesquisaBinariaCandidatos(Candidato[] candidato, String arg) {
-        int inf = 0;
-        int sup = candidato.length - 1;
-        return 0;
+    public static int pesquisaBinariaCandidatos(Candidato[] candidatos, String nome) {
+        int inicio = 0;
+        int fim = candidatos.length - 1;
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+            int comparacao = nome.compareToIgnoreCase(candidatos[meio].getNome());
+
+            if (comparacao == 0) {
+                return  meio;
+            } else if (comparacao < 0) {
+                fim = meio -1;
+            }else{
+                inicio = meio + 1;
+            }
+        }
+        return 1;
     }
 }
