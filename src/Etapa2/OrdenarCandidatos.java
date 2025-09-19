b8b702075a08634ff1b5e9765be1ac52fa5351ea
Projecoes <andrewcoelho2507@gmail.com>
2025-09-19 11:47:22 -0300

    mudando o nome da variavel "chave" para "candidato" para melhor compreensão.

--- src/Etapa2/OrdenarCandidatos.java
+++ src/Etapa2/OrdenarCandidatos.java
@@ -4,42 +4,42 @@ public class OrdenarCandidatos {
 
     public static void ordenaCandidatosPorNome(Candidato[] candidatos) {
         for (int i = 1; i < candidatos.length; i++) {
-            Candidato chave = candidatos[i];
+            Candidato candidato = candidatos[i];
             int j = i - 1;
-            while (j >= 0 && candidatos[j].getNome().compareToIgnoreCase(chave.getNome()) > 0) {
+            while (j >= 0 && candidatos[j].getNome().compareToIgnoreCase(candidato.getNome()) > 0) {
                 candidatos[j + 1] = candidatos[j];
                 j--;
             }
-            candidatos[j + 1] = chave;
+            candidatos[j + 1] = candidato;
         }
     }
 
     public static void ordenaCandidatosPorVotos(Candidato[] candidatos) {
 
         for (int i = 1; i < candidatos.length; i++) {
-            Candidato chave = candidatos[i];
+            Candidato candidato = candidatos[i];
             int j = i - 1;
 
             //candidatos com mais votos primeiro
-            while (j >= 0 && candidatos[j].getIntencoesVotos() < chave.getIntencoesVotos()) {
+            while (j >= 0 && candidatos[j].getIntencoesVotos() < candidato.getIntencoesVotos()) {
                 candidatos[j + 1] = candidatos[j];
                 j--;
             }
-            candidatos[j + 1] = chave;
+            candidatos[j + 1] = candidato;
         }
     }
 
     public static void ordenaCandidatosPorPartido(Candidato[] candidatos) {
 
         for (int i = 1; i < candidatos.length; i++) {
-            Candidato chave = candidatos[i];
+            Candidato candidato = candidatos[i];
             int j = i - 1;
 
-            while (j >= 0 && candidatos[j].getPartido().compareToIgnoreCase(chave.getPartido()) > 0) {
+            while (j >= 0 && candidatos[j].getPartido().compareToIgnoreCase(candidato.getPartido()) > 0) {
                 candidatos[j + 1] = candidatos[j];
                 j--;
             }
-            candidatos[j + 1] = chave;
+            candidatos[j + 1] = candidato;
         }
     }
 
